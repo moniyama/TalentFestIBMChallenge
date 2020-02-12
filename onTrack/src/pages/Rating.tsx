e26e872e9530159acbccf5f1b91e061668d558c1
tiemimaeda <priscila.maeda@yahoo.com.br>
2020-02-12 14:45:42 -0300

    images added

--- onTrack/src/pages/Rating.tsx
+++ onTrack/src/pages/Rating.tsx
@@ -62,9 +62,9 @@ const Rating: React.FC = () => {
   
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader className="header">
         <div className="logo">
-        <img src="/assets/LogoWoman.png"/>
+        <img src="/assets/logo.jpeg"/>
         </div>
       </IonHeader>
         <IonListHeader>
